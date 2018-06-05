49150443758d3924b2e1cdb64fd1c62398195ba5
j40903272 <b04902103@ntu.edu.tw>
2018-06-05 18:57:20 +0800

    rm SiLU

--- hw3/Components/__init__.py
+++ hw3/Components/__init__.py
@@ -5,7 +5,6 @@
 from .Residual import *
 from .Stack import *
 from .ResNetCell import *
-from .SiLU import *
 from .History import *
 from .ResNet_stacks import ResNet_stacks
 from .Upsample_stacks import Upsample_stacks
